f1370d0343c904a9a8bd08ad1e1888134730d692
aeadigt <aea@digt.ru>
2017-02-03 14:24:48 +0300

    Внесены корректировки с определением expires при регистрации абонента

--- tests/default_handlers.js
+++ tests/default_handlers.js
@@ -1,4 +1,31 @@
 'use strict';
 
 let sipServerModule = require('../index.js');
-let sipServer = new sipServerModule.SipServer();
+let settings = {
+    accounts: {
+        '1': {
+            user: '1',
+            password: '1'
+        },
+        'alice': {
+            user: 'alice',
+            password: 'alice'
+        }
+    }
+}
+let sipServer = new sipServerModule.SipServer(settings);
+/*
+setInterval(function() {
+    console.log(' ');
+
+    console.log('sipServer.registry: ', sipServer.registry);
+
+    sipServer.registry.get('1', (err, data) => {
+        console.log('registry 1: ', data);
+    });
+
+    sipServer.registry.get('alice', (err, data) => {
+        console.log('registry alice: ', data);
+    });
+}, 3000);
+*/
