b5b9e5d00866bbabec5a1a53b8734b00938eef88
aea <aea@digt.ru>
2018-06-06 16:30:38 +0300

    Add sip accounts.

--- example/default_handlers.js
+++ example/default_handlers.js
@@ -7,6 +7,22 @@ let settings = {
             user: '1',
             password: '1'
         },
+        '2': {
+            user: '2',
+            password: '2'
+        },
+        '3': {
+            user: '3',
+            password: '3'
+        },
+        '4': {
+            user: '4',
+            password: '4'
+        },
+        '5': {
+            user: '5',
+            password: '5'
+        },
         'alice': {
             user: 'alice',
             password: 'alice'
